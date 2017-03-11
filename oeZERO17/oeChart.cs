cf236d24d0636cee082a99f6a7d0e810e3850bac
Honza S. Copak <bonum.comunae@volny.cz>
2017-03-11 18:32:58 +0100

    Add files via upload

---
+++ oeZERO17/oeChart.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oeChart : MonoBehaviour
+{
+
+    private LineRenderer lineRenderer;
+    private LineRenderer lineRenderer2;
+    private float moveStrength = 0.5f;
+    public GameObject go1;
+    public float side = 5f;
+    public Transform startTrans;
+    public int points = 30;
+    //public Vector3 startV = new Vector3(0,-2,5.5f); 
+   
+
+    // Use this for initialization
+    void Start()
+    {
+        //go1 = GameObject.Find("plug1");
+
+        //transform.Translate(Vector3.forward * (Vector3.Distance(target.position, transform.position)); //Movement slightly towards the object to negate the offset of sideways motion
+        //transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, Time.deltaTime* 1);
+
+
+        
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.numPositions = 5;
+        lineRenderer2 = GetComponent<LineRenderer>();
+        lineRenderer2.numPositions = points* points+ points*2;
+
+        go1.transform.position = startTrans.position;
+
+        SetLine();
+        //SetLine2();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetLine();
+    }
+
+    void SetLine()
+    {
+        lineRenderer.SetPosition(0, go1.transform.position);
+        lineRenderer.SetPosition(1, go1.transform.position + new Vector3(0, side, 0));
+        lineRenderer.SetPosition(2, go1.transform.position + new Vector3(side, side, 0));
+        lineRenderer.SetPosition(3, go1.transform.position + new Vector3(side, 0, 0));
+        lineRenderer.SetPosition(4, go1.transform.position);
+
+        int xp = 5;
+        for (float r = 0; r < points; r++)
+        {
+            for (float x = 0; x < points; x++)
+            {
+
+                lineRenderer2.SetPosition(xp, go1.transform.position + new Vector3(x / 2, Mathf.Sin(x / 2) + Mathf.Cos(r / 2), r));
+                xp++;
+            }
+            lineRenderer2.SetPosition(xp, go1.transform.position + new Vector3(points/2 ,0, r));
+            xp++;
+            lineRenderer2.SetPosition(xp, go1.transform.position + new Vector3(0, 0, 0));
+            xp++;
+        }
+
+        //lineRenderer.SetPosition(5, go1.transform.position + new Vector3(side / 2, 0, 0));
+        //lineRenderer.SetPosition(6, go1.transform.position + new Vector3(side / 2, side, 0));
+        //lineRenderer.SetPosition(7, go1.transform.position + new Vector3(side / 2 + side, side, 0));
+        //lineRenderer.SetPosition(8, go1.transform.position + new Vector3(side / 2 + side, 0, 0));
+        //lineRenderer.SetPosition(9, go1.transform.position + new Vector3(side / 2, 0, 0));
+    }
+
+    /*void SetLine2()
+    {
+        lineRenderer2.SetPosition(0, go1.transform.position + new Vector3(side/2, 0, 0));
+        lineRenderer2.SetPosition(1, go1.transform.position + new Vector3(side/2, side, 0));
+        lineRenderer2.SetPosition(2, go1.transform.position + new Vector3(side/2+side, side, 0));
+        lineRenderer2.SetPosition(3, go1.transform.position + new Vector3(side/2+side, 0, 0));
+        lineRenderer2.SetPosition(4, go1.transform.position + new Vector3(side/2, 0, 0));
+    }*/
+
+}
---
+++ oeZERO17/oeCubePoints.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oeCubePoints : MonoBehaviour
+{
+
+    public int horizontal = 5;
+    public int vertical = 3;
+    //public int size = 1;
+    public float scale = 0.1f;
+
+    private Vector3 position;
+
+    // Use this for initialization
+    void Start()
+    {
+        position = Vector3.zero;
+
+        for (int j = 0; j < horizontal; j++)
+        {
+
+            for (int i = 0; i < vertical; i++)
+            {
+                position.x += scale*2;               
+                GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                go.name = "oeCubePoints" + j +"." + i;
+                go.transform.position = position;
+                go.transform.localScale = new Vector3(scale, scale, scale);
+                go.transform.SetParent(transform);
+            }
+            position.x = 0;
+            position.y += scale*2;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
+
