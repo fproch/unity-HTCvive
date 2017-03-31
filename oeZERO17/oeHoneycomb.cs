98d240c6e2258361babfc095cc33152ae0f24e8a
Honza S. Copak <bonum.comunae@volny.cz>
2017-03-31 22:33:30 +0200

    Add files via upload

--- oeZERO17/oeHoneycomb.cs
+++ oeZERO17/oeHoneycomb.cs
@@ -2,44 +2,83 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class oeHoneycomb : MonoBehaviour {
+public class oeHoneycomb : MonoBehaviour
+{
+
+    GameObject goL1;
+    GameObject goL2;
+    GameObject goL3;
+
+    GameObject goLR;
+    GameObject goLO;
+    GameObject goLY;
+    GameObject goLG;
+    GameObject goLB;
+
 
     GameObject go1;
     GameObject go2;
     GameObject go3; //object_3 = main white
 
+    TextMesh toStatus; //beeTextStatus
     TextMesh to1;
     Renderer rendHoney;
     public int indexHoney = 1;
+    public bool colorLightsVisible = false;
+    public bool mainCylinderVisible = false;
     public bool mainWhiteVisible = true;
     public Color upColor;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         string parentName = transform.root.gameObject.name;
 
         //parent(gameobject)->child(gameobject)--->child(gameobject)
         //gameObject.transform.parent.gameObject.transform.parent.gameObject
+        go1 = getChildGameObject(transform.root.gameObject, "object_1-cylinder");
+        go2 = getChildGameObject(transform.root.gameObject, "object_2-whitebox");
+        go3 = getChildGameObject(transform.root.gameObject, "object_3");
+        if (!mainCylinderVisible) go1.active = false;
+        if (!mainWhiteVisible) go2.active = false;
 
-        Debug.Log("--- oeHoneycomb --- "+ parentName);
+        goLR = getChildGameObject(transform.root.gameObject, "object_LEDR");
+        goLO = getChildGameObject(transform.root.gameObject, "object_LEDO");
+        goLY = getChildGameObject(transform.root.gameObject, "object_LEDY");
+        goLG = getChildGameObject(transform.root.gameObject, "object_LEDG");
+        goLB = getChildGameObject(transform.root.gameObject, "object_LEDB");
+
+        if (!colorLightsVisible) {
+            goLR.active = false;
+            goLO.active = false;
+            goLY.active = false;
+            goLG.active = false;
+            goLB.active = false;            
+        }
+
+
+        Debug.Log("--- oeHoneycomb --- " + parentName);
         //go1 = GameObject.Find("object_1up"); //ok - only first       
         //go1 = transform.Find(parentName + "/object_1up").gameObject; //null
-        go1 = getChildGameObject(transform.root.gameObject, "object_1up");
+        goL1 = getChildGameObject(transform.root.gameObject, "object_L1");
+       
 
+        toStatus = getChildGameObject(transform.root.gameObject, "beeTextStatus").GetComponent<TextMesh>();
+        toStatus.text = "beeTextStatus: "+ parentName + " index: "+ indexHoney;
         //to1 = GameObject.Find("text0").GetComponent<TextMesh>();
-        rendHoney = go1.GetComponent<Renderer>();
+        rendHoney = goL1.GetComponent<Renderer>();
         rendHoney.material.color = upColor; //Color.green;
 
-        go3 = getChildGameObject(transform.root.gameObject, "object_3");
-        if (!mainWhiteVisible) go3.active = false;
-
+       
+        
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     static public GameObject getChildGameObject(GameObject fromGameObject, string withName)
     {
