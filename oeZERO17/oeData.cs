9aa035abe6b6af929035c933a9e5253cca99df4a
Honza S. Copak <bonum.comunae@volny.cz>
2017-04-06 13:48:52 +0200

    Add files via upload

--- oeZERO17/oeData.cs
+++ oeZERO17/oeData.cs
@@ -12,15 +12,25 @@ public class oeData : MonoBehaviour
     string textFile;
     string lineOfText;
     public string dataFile = "data.txt";
-    string[] dataLines; // 
-        
+    string[] dataLines; //
+    string[] lineData;
+    GameObject go;
+    Renderer rend;
+    public int xMinus;
+    public enum NO { Sphere, Cube }
+    public float scaleSize = 0.1f;
+    public NO newObjects;
+    public string nameObj = "dObj";
+    public bool debugList = false;
+
+
     //================================================================================================
     //start-inicializace
     void Start()
     {
-        Debug.Log("oeData.constructor");
+        Debug.Log("oeData.constructor - newObjects: " + newObjects);
         Debug.Log(dataFile);
-        dataLines = new string[255];        
+        dataLines = new string[100000];        
         oeLoadData();
     }
     //------------------------------------------------/start----------------------------------------
@@ -39,6 +49,8 @@ public class oeData : MonoBehaviour
     private void oeLoadData()
     {
         Debug.Log("loadData():");
+
+
         //var file = new System.IO.StreamReader(@"c:\" + data16File, System.Text.Encoding.UTF8, true, 128);
         string sName = Application.loadedLevelName;
         dataFile = Application.dataPath + "/Resources/" + dataFile;
@@ -48,19 +60,43 @@ public class oeData : MonoBehaviour
         while ((lineOfText = file.ReadLine()) != null)
         {
             dataLines[indexLine] = lineOfText;
-            Debug.Log(indexLine.ToString() + " > " + dataLines[indexLine].ToString());
+            if (debugList) Debug.Log(indexLine.ToString() + " > " + dataLines[indexLine].ToString());
+
+            lineData = (dataLines[indexLine].Trim()).Split(";"[0]);
+            go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            try
+            {
+                float xi = float.Parse(lineData[0]) - xMinus;
+                float yi = float.Parse(lineData[0]);
+                float zi = float.Parse(lineData[0]);
+               
+                //Rigidbody gameObjectsRigidBody = go.AddComponent<Rigidbody>(); // Add the rigidbody.
+                //gameObjectsRigidBody.mass = oeMass; // Set the GO's mass to 5 via the Rigidbod
+                //go.GetComponent<Rigidbody>().useGravity = true;
+                go.name = nameObj + "." + indexLine;
+                if (debugList) Debug.Log(go.name);
+
+                go.transform.position = new Vector3(xi, yi, zi);
+                go.transform.localScale = new Vector3(scaleSize, scaleSize, scaleSize);
+                
+            }
+            catch
+            {
+                Debug.Log("Err: try Parse...");
+            }
+
+            rend = go.GetComponent<Renderer>();
             indexLine++;
         }
-        
-        Debug.Log("first dataLines test > " + dataLines[0].ToString());
 
-        string[] lineData;
+
+        Debug.Log("------------ indexLines > " + indexLine);
+        Debug.Log("first dataLines test > " + dataLines[0].ToString());        
         lineData = (dataLines[0].Trim()).Split(";"[0]);
-
         Debug.Log("lineData x test > " + lineData[0]);
-        
-        float x =  float.Parse(lineData[0]);
+        float x =  float.Parse(lineData[0]) - xMinus;
         Debug.Log("lineData x+1 test > " + (x+1).ToString());
-
+       
     }
 }
