1be9fe757e6755d1e8f0c2db6273e67b77121710
Honza S. Copak <bonum.comunae@volny.cz>
2017-05-18 13:08:40 +0200

    Add files via upload

--- oeZERO17/oeKeybSound.cs
+++ oeZERO17/oeKeybSound.cs
@@ -11,12 +11,24 @@ public class oeKeybSound : MonoBehaviour
 
     public bool rndSnd2 = false;
     public bool beatSnd3 = false;
+    public bool beatSnd3pattern = false;
+    public bool pattFade3 = false;
+
+
     public int everyFPS = 500;
     public int volume3 = 50;
 
     public bool debugLog = false;
+    int pi = 0;
     int cntU = 0;
     int cntKbd = 0;
+    int vol3 = 10; // temp volume % 0-100
+
+
+    int[] pattern1 = new int[8] { 1,0,1,0,1,1,1,0 };
+
+
+
 
     // Use this for initialization
     void Start () {
@@ -54,9 +66,24 @@ public class oeKeybSound : MonoBehaviour
 
         if (beatSnd3)
         {
-            if (cntU % everyFPS == 0) play3();
+            if (cntU % everyFPS == 0) play3(volume3);
         }
-        
+
+        if (beatSnd3pattern)
+        {
+            if (cntU % everyFPS == 0)
+            {
+                if (pi > pattern1.Length-1) pi = 0;
+                if (pattFade3) vol3 =volume3;
+                else vol3 = volume3;
+
+                if (pattern1[pi] == 1) play3(100-(pi+1)*10);              
+                
+                pi++;
+            }
+        }
+
+
 
         // movement
         /*
@@ -70,7 +97,7 @@ public class oeKeybSound : MonoBehaviour
         {            transform.Translate(new Vector3(0, 0, -moveStrength));
         }
         */
-        }
+    }
 
     void play2()
     {
@@ -78,11 +105,11 @@ public class oeKeybSound : MonoBehaviour
         snd2.SetActive(true); ///sound1.ok 
     }
 
-    void play3()
+    void play3(int vol)
     {
         snd3.SetActive(false);
         snd3.SetActive(true); ///sound1.ok 
-        snd3.GetComponent<AudioSource>().volume = volume3 * 0.01f; //(float)oeData0 / 100;
+        snd3.GetComponent<AudioSource>().volume = vol * 0.01f; //(float)oeData0 / 100;
     }
 
 
